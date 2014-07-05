39ae17d1641d2e6414854f0b3ab83a081b4018bd
sathish <sathisharthar@gmail.com>
2014-07-05 16:48:55 +0530

    Create youtube.sh

---
+++ youtube.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PATH=/bin:/usr/local/bin:/usr/bin:/bin:/sbin
+export PATH
+homedir=${HOME}
+
+SERVICE=youtube-dl
+
+if ps ax | grep -v grep | grep -v $0 | grep $SERVICE > /dev/null
+
+then
+    echo "`date`: $SERVICE is running, your videos is downloading"
+else
+    echo "$SERVICE is not running and going to run now"
+    touch $homedir/tube
+    youtube-dl -c -a $homedir/tube
+fi
